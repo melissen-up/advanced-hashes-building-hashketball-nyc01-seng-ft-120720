9df877414458f36fdc8a0cc662c1ebb3055b5178
Melissa F <melissa.faulner@gmail.com>
2020-12-03 21:10:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,9 @@
 def game_hash
   {
-    :home: {
-      :team_name: "Brooklyn Nets",
-      :colors: ["Black", "White"],
-      :players: {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
         player_name: "Alan Anderson",
         number: 0,
         shoe: 16,
