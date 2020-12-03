85791591a8a46b9fd5bf4af2afa9e1c38c8ba12e
Melissa F <melissa.faulner@gmail.com>
2020-12-03 21:23:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: {
+      players: [
         player_name: "Alan Anderson",
         number: 0,
         shoe: 16,
@@ -58,11 +58,11 @@ def game_hash
         blocks: 11,
         slam_dunks: 1
       }
-    },
+    ],
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
-      players: {
+      players: [
         player_name: "Jeff Adrien",
         number: 4,
         shoe: 18,
@@ -117,5 +117,6 @@ def game_hash
         blocks: 5,
         slam_dunks: 12
       }
+    ]
   }
 end
